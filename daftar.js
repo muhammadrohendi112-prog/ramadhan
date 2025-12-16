66f49edabb351ade3a1949224a81b0af8d5bc571
muhammadrohendi112-prog <muhammadrohendi112@gmail.com>
2025-12-16 20:41:40 +0700

    Update daftar.js

--- daftar.js
+++ daftar.js
@@ -1,26 +1,28 @@
-
 function kirimWA(e) {
-  e.preventDefault(); // supaya form tidak reload
+  e.preventDefault();
 
-  const nama   = document.getElementById("nama").value;
+  const nama = document.getElementById("nama").value;
+  const nik = document.getElementById("nik").value;
+  const tanggal = document.getElementById("tanggal_lahir").value;
   const alamat = document.getElementById("alamat").value;
-  const wa     = document.getElementById("wa").value;
-  const usia   = document.getElementById("usia").value;
+  const wa = document.getElementById("wa").value;
   const status = document.getElementById("status").value;
 
-  const nomorPanitia = "62895365952208"
+  const nomorPanitia = "62895365952208" // pastikan benar
 
   const pesan =
 `PENDAFTARAN PASARAN RAMADHAN
 ---------------------------
-Nama   : ${nama}
-Alamat : ${alamat}
-No WA  : ${wa}
-Usia   : ${usia}
-Status : ${status}
+Nama          : ${nama}
+NIK           : ${nik}
+Tanggal Lahir : ${tanggal}
+Alamat        : ${alamat}
+No WA         : ${wa}
+Status        : ${status}
 
 Mohon konfirmasi pendaftaran.`;
 
   const url = "https://wa.me/" + nomorPanitia + "?text=" + encodeURIComponent(pesan);
   window.open(url, "_blank");
 }
+
